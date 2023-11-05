766076f988d509e32b90b2722f169520895df0c5
nictru <nictru32@gmail.com>
2023-11-05 08:50:16 +0100

    Add argparser package

--- install.R
+++ install.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
-install.packages("HMM")
+install.packages(c("HMM", "argparser"))
 BiocManager::install(c("IRanges", "GenomicRanges", "bamsignals", "rtracklayer", "Rsamtools", "edgeR", "affyPLM"))
 
 devtools::build()
